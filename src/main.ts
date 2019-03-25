18d046e83025a9f1259c0f507d896ca009df18e1
Dylan Broussard <deraw@outlook.fr>
2019-03-25 18:40:41 +0100

    🔥 Remove useless package

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,9 @@ import App from './App.vue';
 import './filters/';
 import router from './router';
 import store from './store/';
-import VueConfig from 'vue-config';
 import i18n from './i18n';
 
 import 'vue-material-design-icons/styles.css';
-
-import config from '@/config/';
-
-Vue.use(VueConfig, config);
 
 Vue.config.productionTip = false;
 
